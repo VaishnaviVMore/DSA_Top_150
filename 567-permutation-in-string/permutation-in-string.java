22f93934e0e97876d8275f464c3a178e18529d98
Vaishnavi More <07mvaishnavi@gmail.com>
2025-08-08 15:16:08 +0530

    Time: 83 ms (17.12%) | Memory: 45.1 MB (19.54%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,31 +1,32 @@
 class Solution {
-    public boolean matches(int freq[], int freq1[]){
-        for(int i=0;i<26;i++){
-            if(freq[i]!=freq1[i]){
+    public boolean matches(int[] freq, int[] freq1) {
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] != freq1[i]) {
                 return false;
             }
         }
         return true;
     }
-    public boolean checkInclusion(String s1, String s2) {
-        if(s1.length()>s2.length()) return false;
-        int[] freq = new int[26];
 
-        for(int i=0;i<s1.length();i++){
-            freq[s1.charAt(i)-'a']++;
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) return false;
+
+        int[] freq = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            freq[s1.charAt(i) - 'a']++;
         }
-        int windsize=s1.length();
-        for(int i=0;i<s2.length();i++){
-            int windind=0;int ind=i;
+
+        int windsize = s1.length();
+        for (int i = 0; i <= s2.length() - windsize; i++) {
             int[] wind = new int[26];
-            while(windind<windsize && ind<s2.length()){
-                wind[s2.charAt(ind)-'a']++;
-                windind++;ind++;
+            for (int j = 0; j < windsize; j++) {
+                wind[s2.charAt(i + j) - 'a']++;
             }
-            if(matches(freq, wind)){
+            if (matches(freq, wind)) {
                 return true;
             }
         }
+
         return false;
     }
 }
