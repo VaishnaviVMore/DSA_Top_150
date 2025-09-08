6e605305d19ab646ab457775e2022cabd00e8dc5
Vaishnavi More <07mvaishnavi@gmail.com>
2025-09-08 16:49:57 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (98.19%) - LeetSync

---
+++ 2050-count-good-numbers/count-good-numbers.java
@@ -0,0 +1,22 @@
+class Solution {
+    private static final int MOD = 1000000007;
+    public long pow(long base, long ex) {
+        if (ex == 0) return 1;
+        long halfpow = pow(base, ex / 2) % MOD;
+        long result = (halfpow * halfpow) % MOD;
+        if (ex % 2 == 1) {
+            result = (result * base) % MOD;
+        }
+        return result;
+    }
+
+    public int countGoodNumbers(long n) {
+        long odd = n / 2;
+        long even = n / 2 + n % 2;
+
+        long a = pow(5, even);
+        long b = pow(4, odd);
+
+        return (int)((a * b) % MOD);
+    }
+}
