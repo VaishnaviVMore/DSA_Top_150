0649b93ece621a5389414bfcad61982fdcaa68d3
Vaishnavi More <07mvaishnavi@gmail.com>
2025-09-12 15:42:44 +0530

    Time: 34 ms (20.99%) | Memory: 46 MB (43.11%) - LeetSync

---
+++ 575-distribute-candies/distribute-candies.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        Arrays.sort(candyType);
+        int n=candyType.length;
+        int maxtype=1;
+        for(int i=0;i<n-1;i++){
+            if(candyType[i]==candyType[i+1]) continue;
+            else{
+                maxtype++;
+            }
+        }
+        int mintype=n/2;
+        if(mintype>maxtype){
+            return maxtype;
+        }
+        else{
+            return mintype;
+        }
+    }
+}
