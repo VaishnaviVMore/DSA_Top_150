269d384495eaf0d1fa5661f9c5f4360b349ac3a1
Vaishnavi More <07mvaishnavi@gmail.com>
2025-09-16 15:34:27 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (14.87%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -17,15 +17,15 @@ class Solution {
         while(t1 !=null && t2!=null){
             if(t1.val >= t2.val){
                 temp.next=t2;
-                //temp=t2;
+                temp=t2;
                 t2=t2.next;
             }
             else{
                 temp.next=t1;
-                //temp=t1;
+                temp=t1;
                 t1=t1.next;
             }
-            temp=temp.next;
+            //temp=temp.next;
         }
         if(t1!=null){
             temp.next=t1;
