affb8ad2860b7c796511185795cdd8ddd55576b2
Vaishnavi More <07mvaishnavi@gmail.com>
2025-10-09 09:59:19 +0530

    Time: 30 ms (70.97%) | Memory: 52.8 MB (14.99%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,37 +1,36 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
-        List<List<Integer>>main=new ArrayList<>();
         int n=nums.length;
-       for(int i=0;i<n;i++){
-        if(i>0 && nums[i]==nums[i-1])continue;
-            int k=i+1;
-             int j=n-1;
-            while(k<j){
+        Arrays.sort(nums);
+        int i=0;
+        List<List<Integer>>list=new ArrayList<>();
+        for(i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int j=i+1;int k=n-1;
+            while(j<k){
+                 int sum=nums[i]+nums[j]+nums[k];
+            if(sum==0){
                 List<Integer>sub=new ArrayList<>();
-                int sum=nums[i]+nums[j]+nums[k];
-                if(sum==0){
-                    sub.add(nums[i]);
-                    sub.add(nums[j]);
-                    sub.add(nums[k]);
-                    main.add(sub);
-
-                    while(j>k && nums[j]==nums[j-1]){
-                    j--;
+                sub.add(nums[i]);
+                sub.add(nums[j]);
+                sub.add(nums[k]);
+                list.add(sub);
+                while(j<k && nums[j]==nums[j+1]){
+                    j++;
                 }
-                while(j>k && nums[k]==nums[k+1]){
-                    k++;
+                while(j<k && nums[k]==nums[k-1]){
+                    k--;
                 }
-                j--;k++;
-                }
-                else if(sum>0){
-                    j--;
-                }
-                else{
-                    k++;
-                }
+                j++;k--;
             }
+            else if(sum>0){
+                k--;
+            }
+            else{
+                j++;
+            }
+            } 
         }
-        return main;
+        return list;
     }
 }
