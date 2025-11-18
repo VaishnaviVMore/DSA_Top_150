0d4924952c90a011469655fdda95ae79fe6f9dad
Vaishnavi More <07mvaishnavi@gmail.com>
2025-11-18 16:29:38 +0530

    Time: 2 ms (8.04%) | Memory: 45.5 MB (5.05%) - LeetSync

--- 2710-minimum-operations-to-reduce-an-integer-to-0/minimum-operations-to-reduce-an-integer-to-0.java
+++ 2710-minimum-operations-to-reduce-an-integer-to-0/minimum-operations-to-reduce-an-integer-to-0.java
@@ -1,12 +1,22 @@
 class Solution {
     public int minOperations(int n) {
-        if(n==0 || n==1){
+        int[] dp=new int[n+1];
+        Arrays.fill(dp, -1);
+        return solve(n,dp);
+    }
+    public static int solve(int n, int dp[]){
+        if(n==0 ||n==1){
             return n;
         }
+        if(dp[n]!=-1)return dp[n];
+
         int val=1;
         while(val*2<n){
             val=val*2;
         }
-        return 1+Math.min(minOperations(val*2-n),minOperations(n-val));
+        int add=solve(val*2-n,dp);
+        int sub=solve(n-val,dp);
+        dp[n]=1+Math.min(add,sub);
+        return dp[n];
     }
 }
