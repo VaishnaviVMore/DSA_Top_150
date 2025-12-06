eac3e53dea35c19929c34b0826f29416297e7a7e
Vaishnavi More <07mvaishnavi@gmail.com>
2025-12-06 17:36:26 +0530

    Time: 8 ms (29.07%) | Memory: 63.1 MB (41.66%) - LeetSync

---
+++ 1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.java
@@ -0,0 +1,32 @@
+class Solution {
+    int result = 0;   // GLOBAL variable
+
+    public int maxSumBST(TreeNode root) {
+        helper(root);
+        return result;
+    }
+
+    // returns {minValue, maxValue, sum}
+    public int[] helper(TreeNode node) {
+        if (node == null) {
+            return new int[]{Integer.MAX_VALUE, Integer.MIN_VALUE, 0};
+        }
+
+        int[] left = helper(node.left);
+        int[] right = helper(node.right);
+
+        // if not BST
+        if (left[1] >= node.val || right[0] <= node.val) {
+            return new int[]{Integer.MIN_VALUE, Integer.MAX_VALUE, 0};
+        }
+
+        int sum = left[2] + node.val + right[2];
+        result = Math.max(result, sum);
+
+        return new int[]{
+            Math.min(node.val, left[0]),
+            Math.max(node.val, right[1]),
+            sum
+        };
+    }
+}
